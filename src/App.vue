cf1e16bf73ac402c4f2db3911ec31986be4dd437
mouhcine <mouhcine15.00@gmail.com>
2022-06-14 16:16:43 +0200

    partenza

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MovieList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MovieList from './components/MovieList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MovieList
   }
 }
 </script>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'MovieList',
+
+    data () {
+      return {
+          apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=6e5cb1250b73157692bdf4c00483dc2b&language=it-IT&query=matrix",
+          movies: []
+      }
+    },
+    created() {
+      axios.get(this.apiUrl)
+      .then(result => {
+          this.movies = result.data.response;
+          console.log(result)
+      })
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #hello {
+        background-color: aqua;
+    }
+</style>
